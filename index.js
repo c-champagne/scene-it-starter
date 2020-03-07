5f8b190aaa84def06d0f8f55747a4681d2c1ee20
c-champagne <cassielchampagne@gmail.com>
2020-03-07 11:44:27 -0500

    Finished template literal, working on getting preventDefault to work correctly

---
+++ index.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+    function renderMovies(movieArray) {
+        var movieHTML = [];
+        movieHTML = movieArray.map(function (currentMovie) {
+            return `
+            <div class="col-3 card movie">
+						<img class="card-img-top poster" src="${currentMovie.Poster}" />
+						<h4 class="movieTitle">${currentMovie.Title}</h4>
+						<h5 class="releaseDate">${currentMovie.Year}</h5>
+						<button class="add">Add</button>
+					</div>
+            `
+        })
+        return movieHTML.join("")
+    }
+    var moviesContainer = document.getElementById("movies-container");
+    moviesContainer.innerHTML = renderMovies(movieData);
+    
+    
+    document.getElementById("myForm").addEventListener("submit", function(e){
+        e.preventDefault();
+        
+    })
+
+    
+});
